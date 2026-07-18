24f13da5a68f05ad7766b34e5ec3e1b29c2a4ffc
valeria.grubrina <valeriagrubrina@gmail.com>
2026-07-18 21:59:22 +0300

    Добавление options.addArguments("--headless=new");

--- src/test/java/dev/valvar/orangehrm/test/BaseTest.java
+++ src/test/java/dev/valvar/orangehrm/test/BaseTest.java
@@ -43,6 +43,7 @@ public abstract class BaseTest {
             options.addArguments("--incognito");
             options.addArguments("--lang=en-US");
             options.addArguments("--disable-notifications");
+            options.addArguments("--headless=new");
             Configuration.browserCapabilities = options;
         }
 
